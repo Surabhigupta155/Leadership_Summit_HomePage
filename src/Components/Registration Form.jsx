9561fe535dd2156e90495339b30dd06fc5297294
bhavya132 <2019uce0056@iitjammu.ac.in>
2021-10-11 18:48:40 +0530

    Update Timeline

--- src/Components/Registration Form.jsx
+++ src/Components/Registration Form.jsx
@@ -30,45 +30,7 @@ export default function Registration() {
               </div>
             </ScrollAnimation>
           </div>
-          {/* <ScrollAnimation animateIn="fadeInDown">
-            <div className="text-center heading mb-lg-5 mb-md-5 mb-4">
-              {" "}
-              <h1>Registration Form</h1>
-            </div>
-          </ScrollAnimation>
-           <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            style={{ minHeight: "10vh" }}
-          >
-            <Grid item xs={3}>
-              <ScrollAnimation animateIn="fadeInLeft">
-                <iframe
-                  src="https://docs.google.com/forms/d/e/1FAIpQLSdPMQ-KiaUjtAFYN3mL4RMvhJBJWE0y-3DkhhcVzNLQOzcjkw/viewform?embedded=true"
-                  width="600"
-                  height="1600"
-                  frameborder="0"
-                  marginheight="0"
-                  marginwidth="-2%"
-                >
-                  Loading…
-                </iframe>
-              </ScrollAnimation>
-            </Grid>
-          </Grid> */}
-          
-          {/* <div className="justify-content-center align-items-center mx-0">
-                            <div>
-                                <ScrollAnimation animateIn="fadeInLeft">
-                                    <h1>Registration Form</h1>
-                                    </ScrollAnimation>
-                            </div>
-                           
-                        </div>
-                   */}
+         
         </div>
       </section>
     </div>
